7491861e21a1fa90fbc82fa596dcdd9cee78c1f8
kimjoin2@gmail.com <kimjoin2@gmail.com>
2017-06-07 09:31:18 +0900

    code for make excel file whith class PracticeForExcel

---
+++ src/main/PracticeForExcel.java
@@ -0,0 +1,54 @@
+package main;
+
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Created by bumskim on 2017. 6. 7..
+ */
+public class PracticeForExcel {
+
+    static public void main(String[] args) {
+
+        // Create a new excel file which name is "kim's sheet"
+        Workbook wb = new XSSFWorkbook();
+        CreationHelper createHelper = wb.getCreationHelper();
+        Sheet sheet = wb.createSheet("kim's sheet");
+
+        // Create a row and put some cells in it. Rows are 0 based.
+        Row row = sheet.createRow((short)0);
+        // Create a cell and put a value in it.
+        Cell cell = row.createCell(0);
+        cell.setCellValue(1);
+
+        // Or do it on one line.
+        row.createCell(1).setCellValue(1.2);
+        row.createCell(2).setCellValue(createHelper.createRichTextString("This is a string"));
+        row.createCell(3).setCellValue(true);
+
+        String str = "kim's cell";
+        row.createCell(4).setCellValue(str);
+
+        // Write the output to a file
+        FileOutputStream fileOut = null;
+        try {
+            fileOut = new FileOutputStream("src/excel/workbook.xls");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            wb.write(fileOut);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            fileOut.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
